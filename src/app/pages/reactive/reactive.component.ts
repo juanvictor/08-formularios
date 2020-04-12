d1da433d319286ade8b0ced4849f31a4158999f1
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-04-12 13:54:18 -0400

    Clase 201. Formulario Reactivo: Agrupaciones de los objetos - formGroupName.

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -33,9 +33,13 @@ export class ReactiveComponent implements OnInit {
 
   crearFormulario() {
     this.forma = this.fb.group({
-      nombre  : ['', [ Validators.required, Validators.minLength(5) ] ],
-      apellido: ['', Validators.required ],
-      correo  : ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ] ]
+      nombre   : ['', [ Validators.required, Validators.minLength(5) ] ],
+      apellido : ['', Validators.required ],
+      correo   : ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ] ],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required ],
+        ciudad  : ['', Validators.required ]
+      })
     });
   }
 
