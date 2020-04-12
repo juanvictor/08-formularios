0128cd79a89e3b6604eacbe4fd38c3ef88706e69
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-04-12 14:52:48 -0400

    Clase 204. Formulario Reactivo: Arreglos de FormControl - FormArray.

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -18,6 +18,10 @@ export class ReactiveComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  get pasatiempos () {
+    return this.forma.get('pasatiempos') as FormArray;
   }
 
   get nombreNoValido() {
@@ -48,7 +52,10 @@ export class ReactiveComponent implements OnInit {
       direccion: this.fb.group({
         distrito: ['', Validators.required ],
         ciudad  : ['', Validators.required ]
-      })
+      }),
+      pasatiempos: this.fb.array([
+        [], [], [], [], []
+      ])
     });
   }
 
