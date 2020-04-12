8125680146a7daef40d1eca62f7bc08f78c8815e
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-04-12 14:05:00 -0400

    Clase 202. Formulario Reactivo: Validación visual de los campos anidados.

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -31,6 +31,14 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('correo').invalid && this.forma.get('correo').touched;
   }
 
+  get distritoNoValido() {
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
+  }
+
+  get ciudadNoValido() {
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
+  }
+
   crearFormulario() {
     this.forma = this.fb.group({
       nombre   : ['', [ Validators.required, Validators.minLength(5) ] ],
@@ -48,7 +56,11 @@ export class ReactiveComponent implements OnInit {
 
     if ( this.forma.invalid ) {
       return Object.values( this.forma.controls ).forEach( control => {
-        control.markAsTouched();
+        if ( control instanceof FormGroup ) {
+          Object.values( control.controls ).forEach( control1 => control1.markAsTouched() );
+        } else {
+          control.markAsTouched();
+        }
       });
     }
   }
