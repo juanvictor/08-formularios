ffecc04a3fff7b539c48300ac9e440fa0c8a74b4
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-04-12 13:27:16 -0400

    Clase 199. Formulario Reactivo: Validaciones sincronas.

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -21,9 +21,9 @@ export class ReactiveComponent implements OnInit {
 
   crearFormulario() {
     this.forma = this.fb.group({
-      nombre  : ['Juan'],
-      apellido: ['Chavez'],
-      correo  : ['juan@gmail.com']
+      nombre  : ['', [ Validators.required, Validators.minLength(5) ] ],
+      apellido: ['', Validators.required ],
+      correo  : ['', Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ]
     });
   }
 
