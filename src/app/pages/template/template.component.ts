f36ef69315031dc35c31532576eae2352543c98b
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-04-12 10:55:49 -0400

    Clase 194. Validaciones y consumir servicio paises

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { PaisService } from '../../services/pais.service';
+
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
@@ -9,15 +11,30 @@ import { NgForm } from '@angular/forms';
 export class TemplateComponent implements OnInit {
 
   usuario = {
-    nombre: ''
+    nombre  : 'Juan',
+    apellido: 'Chavez',
+    correo  : 'juan@gmail.com'
   };
 
-  constructor() { }
+  constructor(
+    private paisService: PaisService
+  ) { }
 
   ngOnInit(): void {
+    this.paisService.getPaises()
+      .subscribe( paises => {
+        console.log(paises);
+      });
   }
 
   guardar( forma: NgForm ) {
+    if ( forma.invalid ) {
+      Object.values( forma.controls ).forEach( control => {
+        control.markAsTouched();
+      });
+      return;
+    }
+
     console.log(forma);
     console.log(forma.value);
   }
---
+++ src/app/services/pais.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getPaises() {
+    // === FORMA LARGA ===
+
+    // return this.http.get('https://restcountries.eu/rest/v2/lang/es')
+    // .pipe(
+    //   map( (resp: any[]) =>{
+    //     return resp.map( pais => {
+    //       return {
+    //         nombre: pais.name,
+    //         codigo: pais.alpha3Code
+    //       };
+    //     });
+    //   })
+    // );
+
+    // === FORMA CORTA ===
+
+    return this.http.get('https://restcountries.eu/rest/v2/lang/es')
+    .pipe(
+      map( (resp: any[]) =>
+        resp.map( pais => ({ nombre: pais.name, codigo: pais.alpha3Code }) )
+      )
+    );
+  }
+}
