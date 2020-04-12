e81379aa5598bd9d9aa53c00796a04f1b5f42cdd
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-04-12 13:41:03 -0400

    Clase 200. Formulario Reactivo: Validaciones y HTML.

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -19,16 +19,34 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+
+  get apellidoNoValido() {
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  }
+
+  get correoNoValido() {
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+
   crearFormulario() {
     this.forma = this.fb.group({
       nombre  : ['', [ Validators.required, Validators.minLength(5) ] ],
       apellido: ['', Validators.required ],
-      correo  : ['', Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ]
+      correo  : ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ] ]
     });
   }
 
   guardar() {
     console.log(this.forma);
+
+    if ( this.forma.invalid ) {
+      return Object.values( this.forma.controls ).forEach( control => {
+        control.markAsTouched();
+      });
+    }
   }
 
 }
